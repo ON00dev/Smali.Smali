08e55392b021385d5ef66e979e43fc2e9d3b71a8
root <vicguioliveira2005@gmail.com>
2024-06-13 23:03:20 -0400

    Script

--- SmaliSmali.py
+++ SmaliSmali.py
@@ -7,10 +7,10 @@ import logging
 from typing import Optional
 from colorama import init, Fore
 
-# Inicialize colorama
+# Inicialize o colorama
 init(autoreset=True)
 
-# Clean terminal
+# Limpar terminal
 operacional_sys = os.name
 if operacional_sys == "posix":
     os.system("clear")
@@ -40,7 +40,7 @@ print(Fore.BLUE + '''
 -------------------------------------------------------------------------------------------
 ''')
 
-# Configure logging
+# Configure o logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def verify_file_path(file_path: str) -> None:
@@ -72,7 +72,9 @@ def unzip_apk(apk_path: str, output_dir: str) -> None:
 
 def decompile_dex_to_smali(dex_path: str, output_dir: str) -> None:
     create_output_dir(output_dir)
-    command = ['java', '-jar', './utils/baksmali.jar', 'd', dex_path, '-o', output_dir]
+    baksmali_jar_path = './utils/baksmali-2.5.2.jar'
+    verify_file_path(baksmali_jar_path)
+    command = ['java', '-jar', baksmali_jar_path, 'd', dex_path, '-o', output_dir]
     logging.debug(f"Running command: {' '.join(command)}")
     try:
         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -81,45 +83,32 @@ def decompile_dex_to_smali(dex_path: str, output_dir: str) -> None:
         logging.error(f"{Fore.RED}Error decompiling DEX: {e.stderr.decode()}")
         raise RuntimeError(f"Error decompiling DEX: {e.stderr.decode()}")
 
-def save_code_to_file(code: str, file_path: str) -> None:
-    try:
-        with open(file_path, 'w') as file:
-            file.write(code)
-        logging.info(f"{Fore.GREEN}Code saved to {file_path}")
-    except IOError as e:
-        logging.error(f"{Fore.RED}Error saving code to file: {e}")
-        raise RuntimeError(f"Error saving code to file: {e}")
-
 if __name__ == "__main__":
     apk_path: str = input("Enter the path to the APK file: ").strip()
 
     if not apk_path:
         logging.critical(f"{Fore.RED}APK path cannot be empty.")
         sys.exit(1)
-        
-    # Extracting APK's name (no extention) to use as directory name
+
+    # Extrair o nome do arquivo APK (sem extensão) para usar como nome do diretório
     apk_name = os.path.splitext(os.path.basename(apk_path))[0]
     extracted_dir = f'./{apk_name}_extracted'
     smali_output_dir = f'./{apk_name}_smali_output'
 
     try:
-        # Check and extract APK
+        # Verificar e extrair o APK
         verify_file_path(apk_path)
         unzip_apk(apk_path, extracted_dir)
 
-        # Find classes.dex archive
+        # Encontrar o arquivo classes.dex
         dex_path: str = os.path.join(extracted_dir, 'classes.dex')
         verify_file_path(dex_path)
 
-        # Decompile DEX to Smali
+        # Decompilar o arquivo DEX para Smali
         decompile_dex_to_smali(dex_path, smali_output_dir)
-
-        # Opcional: Save Smali code in an archive
-        save: str = input("Do you want to save the Smali code to a .smali file? (y/n): ").strip().lower()
-        if save == 'y':
-            smali_file_path: str = os.path.join(smali_output_dir, "code.smali")
-            smali_code: str = "\n".join(open(os.path.join(smali_output_dir, f)).read() for f in os.listdir(smali_output_dir) if f.endswith('.smali'))
-            save_code_to_file(smali_code, smali_file_path)
+        
+        # Finalizar Programa
+        print(Fore.GREEN+"\nSmali Extraction Completed!")
 
     except FileNotFoundError as e:
         logging.critical(f"{Fore.RED}{e}")
